650cf6ef85ef42c45e7565b6b9ae2bbc6c944619
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-03-28 14:00:23 -0300

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,3 +5,33 @@ function $(element){
 $("#inputMore").addEventListener('click', ()=>{
     window.scroll({top: 100, behavior: "smooth"})
 })
+let conterBg = 1
+
+function updateHome(){
+
+    setTimeout(()=>{
+        $("#containerCarousel").style.marginLeft = "-100vw"
+        setTimeout(()=>{
+            $("#containerCarousel").style.transition = "0s"
+            $("#divBg1").style.backgroundImage = `url("./assets/img/card${conterBg + 1}.jpeg")` 
+            $("#containerCarousel").style.marginLeft = "0px"
+            if(conterBg<5){
+                $("#divBg2").style.backgroundImage = `url("./assets/img/card${conterBg + 2}.jpeg")` 
+            }else {
+                $("#divBg2").style.backgroundImage = `url("./assets/img/card1.jpeg")` 
+            }
+            setTimeout(()=>{
+                $("#containerCarousel").style.transition = "2s"
+                if(conterBg>4){
+                    conterBg = 0
+                } else {
+                    conterBg++
+                }
+                updateHome()
+            },100)
+        },2000)
+
+    },2500)
+}
+
+updateHome()
